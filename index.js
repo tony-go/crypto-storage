125ffbcee78d7d85937b9d2bf3ecad4d8f40b1b6
tony-go <gorez.tony@gmail.com>
2021-06-06 15:16:56 +0200

    feat: add session recovery if no closed before gc

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const {crypto, utils, constant} = require('./lib')
+const {LAST_ONLINE_USER_KEY} = require('./lib/constant')
 
 const {getDerivedKey, getIv, getEncryptedEntity} = crypto
 const {
@@ -9,14 +10,10 @@ const {
   PASSWORD_LENGTH,
   NAME_LENGTH,
   TABLE_NAME_KEY,
+  // ONLINE_TABLE_NAME_KEY,
 } = constant
-const {
-  encode,
-  decode,
-  arrayBufferToBase64,
-  base64ToArrayBuffer,
-  hashString,
-} = utils
+const {encode, decode, arrayBufferToBase64, base64ToArrayBuffer, hashString} =
+  utils
 
 class CryptoStorage {
   /**
@@ -33,47 +30,57 @@ class CryptoStorage {
 
   async create() {
     const {password, name} = this._ctx
-  
+
     if (!name || typeof name !== 'string' || name.length < NAME_LENGTH) {
       throw new Error(`name should be a string of ${NAME_LENGTH} characters`)
     }
-  
+
     if (
       !password ||
       typeof password !== 'string' ||
       password.length < PASSWORD_LENGTH
     ) {
-      throw new Error(`password should be a string of ${PASSWORD_LENGTH} characters`)
+      throw new Error(
+        `password should be a string of ${PASSWORD_LENGTH} characters`,
+      )
     }
-    
+
     this._setTableName(name)
+    this._storage.setItem(LAST_ONLINE_USER_KEY, JSON.stringify(this._ctx))
     this._userName = await getEncryptedEntity(name)
     this._userPassword = await getEncryptedEntity(password)
   }
-  
-  async use () {
+
+  async use() {
+    if (!this._ctx) {
+      await this._openPreviousSession()
+      return
+    }
+
     const {password, name} = this._ctx
-  
+
     this._checkTableName()
-  
+
     this._userName = await getEncryptedEntity(name)
     this._userPassword = await getEncryptedEntity(password)
   }
-  
-  async setItem (key, value) {
+
+  async setItem(key, value) {
     if (!this._userPassword || !this._userName)
       throw new Error('CryptoStorage instance is not ready')
     if (!key || !value)
-      throw new Error('key (String) and value (String | Array) args are required')
+      throw new Error(
+        'key (String) and value (String | Array) args are required',
+      )
     if (typeof key !== 'string')
       throw new Error('key (String) arg should be a string')
-  
+
     const hashedKey = hashString(key + this._userName)
     const bufferValue = encode(value)
     const iv = getIv(this._storage)
     const algorithm = {name: 'AES-GCM', iv}
     const derivedKey = await getDerivedKey(this._userPassword, this._storage)
-  
+
     const cryptoValue = await window.crypto.subtle.encrypt(
       algorithm,
       derivedKey,
@@ -81,17 +88,17 @@ class CryptoStorage {
     )
     const formattedCryptoValue = arrayBufferToBase64(cryptoValue)
     const unEncryptedData = {[key]: value}
-  
+
     this._storage.setItem(hashedKey, formattedCryptoValue)
     return unEncryptedData
   }
-  
-  async getItem (key) {
+
+  async getItem(key) {
     if (!this._userPassword)
       throw new Error('CryptoStorage instance is not ready')
     if (!key || typeof key !== 'string')
       throw new Error('key arg (String) is required')
-  
+
     const hashedKey = hashString(key + this._userName)
     const item = this._storage.getItem(hashedKey)
     if (!item) {
@@ -107,62 +114,78 @@ class CryptoStorage {
       derivedKey,
       base64Value,
     )
-  
+
     return decode(bufferValue)
   }
-  
-  removeItem (key) {
+
+  removeItem(key) {
     if (!this._userPassword)
       throw new Error('CryptoStorage instance is not ready')
     if (!key || typeof key !== 'string')
       throw new Error('key arg (String) is required')
     if (key === SALT_KEY || key === IV_KEY) throw new Error('unsafe operation')
-  
+
     const hashedKey = hashString(key + this._userName)
     const item = this._storage.getItem(hashedKey)
-  
+
     if (!item) {
       console.error(`key '${key}' are not store in the CryptoStorage instance`)
       return null
     }
-  
+
     this._storage.removeItem(hashedKey)
     return key
   }
-  
-  close () {
+
+  close() {
     this._userPassword = null
     this._userName = null
+    this._storage.removeItem(LAST_ONLINE_USER_KEY)
   }
-  
-  _checkStorage () {
+
+  _checkStorage() {
     if (!window || !window.localStorage) {
       throw new Error('localStorage is not available for now')
     }
     this._storage = window.localStorage
   }
-  
-  _checkTableName () {
+
+  async _openPreviousSession() {
+    const rawCtx = this._storage.getItem(LAST_ONLINE_USER_KEY)
+    if (!rawCtx) {
+      throw new Error('This not previous session stored')
+    }
+
+    const ctx = JSON.parse(rawCtx)
+    this._ctx = ctx
+    this._userName = await getEncryptedEntity(ctx.name)
+    this._userPassword = await getEncryptedEntity(ctx.password)
+    return true
+  }
+
+  _checkTableName() {
     const {name} = this._ctx
     const rawTable = this._storage.getItem(TABLE_NAME_KEY)
     const hashName = hashString(name)
     const tableName = JSON.parse(rawTable)
-  
+
     if (!rawTable || !tableName.includes(hashName)) {
-      throw new Error('This name is not registered, you should create an instance instead')
+      throw new Error(
+        'This name is not registered, you should create an instance instead',
+      )
     }
     return true
   }
-  
-  _setTableName (name) {
+
+  _setTableName(name) {
     const rawTable = this._storage.getItem(TABLE_NAME_KEY)
     const hashName = hashString(name)
-  
+
     if (!rawTable) {
       this._storage.setItem(TABLE_NAME_KEY, JSON.stringify([hashName]))
     } else {
       const tableName = JSON.parse(rawTable)
- 
+
       if (!Array.isArray(tableName)) {
         throw new Error(
           'Table name was corrupted you should clear storage and kill instances',
--- test.js
+++ test.js
@@ -23,62 +23,83 @@ vhs('scenario #1: basic usage', async t => {
   t.equal(foo2, 'bar', 'get the item after closing the instance')
 
   // clean up the storage
-  window.localStorage.clear();
+  window.localStorage.clear()
 })
 
-vhs('scenario #1: cannot create two instance with same context (pw, name)', async t => {
-  // create a new storage
-  const storage = new CryptoStorage({name: 'g-ray', password: 'password'})
-  await storage.create()
-
-  // create it again
-  try {
-    await storage.create()
-  } catch (error) {
-    t.equal(error.message, "Name g-ray is already used", 'should throw')
-  }
-
-  // clean up the storage
-  window.localStorage.clear();
-})
-
-vhs('scenario #3: cannot create an instance without a name or a too small password', async t => {
-  const storage = new CryptoStorage({name: '', password: 'password'})
-
-  try {
+vhs(
+  'scenario #2: cannot create two instance with same context (pw, name)',
+  async t => {
     // create a new storage
+    const storage = new CryptoStorage({name: 'g-ray', password: 'password'})
     await storage.create()
-  } catch (error) {
-    t.equal(error.message, "name should be a string of 1 characters", 'no name')
-  }
 
-  const storage2 = new CryptoStorage({name: 'n', password: ''})
+    // create it again
+    try {
+      await storage.create()
+    } catch (error) {
+      t.equal(error.message, 'Name g-ray is already used', 'should throw')
+    }
 
-  try {
-    // create a new storage
-    await storage2.create()
-  } catch (error) {
-    t.equal(error.message, "password should be a string of 5 characters", 'no name')
-  }
-})
+    // clean up the storage
+    window.localStorage.clear()
+  },
+)
 
-vhs('scenario 3: first try with a not valid password should not push name in table name', async t => {
-  let storage = new CryptoStorage({name: 'toto', password: 'pass'})
+vhs(
+  'scenario #3: cannot create an instance without a name or a too small password',
+  async t => {
+    const storage = new CryptoStorage({name: '', password: 'password'})
 
-  try {
+    try {
+      // create a new storage
+      await storage.create()
+    } catch (error) {
+      t.equal(
+        error.message,
+        'name should be a string of 1 characters',
+        'no name',
+      )
+    }
+
+    const storage2 = new CryptoStorage({name: 'n', password: ''})
+
+    try {
+      // create a new storage
+      await storage2.create()
+    } catch (error) {
+      t.equal(
+        error.message,
+        'password should be a string of 5 characters',
+        'no name',
+      )
+    }
+  },
+)
+
+vhs(
+  'scenario #4: first try with a not valid password should not push name in table name',
+  async t => {
+    let storage = new CryptoStorage({name: 'toto', password: 'pass'})
+
+    try {
+      await storage.create()
+    } catch (error) {
+      t.equal(
+        error.message,
+        'password should be a string of 5 characters',
+        'should fail at first try',
+      )
+    }
+
+    storage = new CryptoStorage({name: 'toto', password: 'password'})
     await storage.create()
-  } catch (error) {
-    t.equal(error.message, 'password should be a string of 5 characters', 'should fail at first try')
-  }
+    await storage.setItem('foo', 'bar')
+    const item = await storage.getItem('foo')
+    t.equal(item, 'bar', 'crypto storage instance work')
+  },
+)
 
-  storage = new CryptoStorage({name: 'toto', password: 'password'})
-  await storage.create()
-  await storage.setItem('foo', 'bar')
-  const item = await storage.getItem('foo')
-  t.equal(item, 'bar', 'crypto storage instance work')
-})
-
-vhs('scenario 4: cannot create two instance with a same name', async t => {
+vhs('scenario #5: cannot create two instance with a same name', async t => {
   const storage = new CryptoStorage({name: 'newName', password: 'password'})
   await storage.create()
 
@@ -86,10 +107,46 @@ vhs('scenario 4: cannot create two instance with a same name', async t => {
   const item = await storage.getItem('foo')
   t.equal(item, 'bar', 'crypto storage instance work')
 
-  const otherStorage = new CryptoStorage({name: 'newName', password: 'password2'})
+  const otherStorage = new CryptoStorage({
+    name: 'newName',
+    password: 'password2',
+  })
   try {
     await otherStorage.create()
   } catch (error) {
-    t.equal(error.message, 'Name newName is already used', 'could not create two instance with the same name')
+    t.equal(
+      error.message,
+      'Name newName is already used',
+      'could not create two instance with the same name',
+    )
   }
 })
+
+vhs(
+  'scenario #6: open a session without name and found the previous one (not closed)',
+  async t => {
+    // create a new storage
+    let storage = new CryptoStorage({name: 'g-ray', password: 'password'})
+    await storage.create()
+
+    // add and get an item
+    await storage.setItem('foo', 'bar')
+    const foo = await storage.getItem('foo')
+    t.equal(
+      foo,
+      'bar',
+      'get the item before destroying the instance (without .close)',
+    )
+
+    // the session is deleted but not closed
+    storage = null
+
+    // open it again
+    const sameStorage = new CryptoStorage()
+    await sameStorage.use()
+
+    // get the item
+    const foo2 = await sameStorage.getItem('foo')
+    t.equal(foo2, 'bar', 'get the item after destroying previous instance')
+  },
+)
